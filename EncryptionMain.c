27affa0f4112f5f428e77dcdfea1288e39e90a67
Nicky Grigg <griggnicky@gmail.com>
2022-11-03 08:52:23 -0400

    Set up the outline for the project

---
+++ EncryptionMain.c
@@ -0,0 +1,16 @@
+/*
+
+This program takes in a file as either a .txt or .crp file and encrypts or decrypts the file
+Written by: Nicholas Grigg and Jaccob Pierog
+
+*/
+
+
+#include <stdio.h>
+
+
+int main(void) {
+    
+    
+    return 0;
+}
