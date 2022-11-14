df35107ec8e4c3e4e0c6c97fada26650c80eb2ee
Nicky Grigg <griggnicky@gmail.com>
2022-11-14 09:42:26 -0500

    thing

--- EncryptionMain.c
+++ EncryptionMain.c
@@ -18,6 +18,7 @@ Written by: Nicholas Grigg and Jaccob Pierog
 
 #include <stdio.h>
 #include <string.h>
+char* encrypt(char);
 
 
 int main(int argc, char *argv[]) {
@@ -25,13 +26,23 @@ int main(int argc, char *argv[]) {
     int fileNameLocation, encryptOrNot, sizeOfFile;
     FILE *in, *out;
     
-
-    if (strstr(argv[1], "-d") != NULL|| strstr(argv[2], "-d") != NULL) { // decrypting
-        if (strstr(argv[1], "-d") == NULL) {
+    
+    if (argc-1) {
+        fileNameLocation = 1;
+    } else {
+        if (strstr(argv[1], "-") == NULL) {
             fileNameLocation = 1;
         } else {
             fileNameLocation = 2;
         }
+    }
+
+    printf("%i\n", fileNameLocation);
+
+
+    if (strstr(argv[1], "-d") != NULL|| ((argc > 0) && (strstr(argv[2], "-d") != NULL))) { // decrypting
+    printf("decrypting \n");
+        
         strcpy(renamefilearray, argv[fileNameLocation]);
         renamefilepointer = strtok(renamefilearray, ".");
         rename(argv[fileNameLocation], strcat(renamefilepointer, ".txt"));
@@ -39,15 +50,12 @@ int main(int argc, char *argv[]) {
 
 
     } else { // encrypt
-        if (strstr(argv[2], "-e") == NULL) {
-            fileNameLocation = 2;
-        } else {
-            fileNameLocation = 1;
-        }
+    printf("Here \n");
         strcpy(renamefilearray, argv[fileNameLocation]);
         encryptOrNot = 1;
     }
 
+    
     in = fopen(renamefilepointer, "r");
     out = fopen("tempFile.txt", "w"); // used to tempererarely write to and then will be renamed to replace the in file
     
@@ -71,8 +79,7 @@ int main(int argc, char *argv[]) {
         if (inputStream == EOF) {
             break;
         }
-        // decrypt(inputStream);
-        // need to write what is returned to the file
+        fprintf(out, "%s", encrypt(fgetc(in)));
         }
     }
 
@@ -89,5 +96,36 @@ int main(int argc, char *argv[]) {
         remove(argv[fileNameLocation]); // deletes the .txt file
     }
 
+
+
+
     return 0;
+}
+
+char* encrypt(char input){
+    char temp[2];
+    char *item = temp;
+
+
+    if (input == 9) {
+        return "TT";
+    } else if (input == 13) {
+        return "\n";
+    } else {
+        input-=16;
+        if (input<32) {
+            input = (input-32 + 144);
+        }
+        temp[0] = input/16 + 48;
+        temp[1] = input%16;
+        if (temp[1] > 9) {
+            temp[1]+=55;
+        } else {
+            temp[1] +=48;
+        }
+
+    
+    }
+    return item;
+    
 }
